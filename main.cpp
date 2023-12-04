77145a8edd6f70b22d6bebaa98f969c3514a2e8f
Vinicius Antonio da Silva <vinicius.antonio0712@gmail.com>
2023-12-04 14:14:43 -0300

    refactor and added players

--- main.cpp
+++ main.cpp
@@ -13,6 +13,12 @@ int main () {
     Vector2 ball_position = {200, 200};
     Vector2 ball_velocity = {1, 1};
 
+    int rect_width = 30;
+    int rect_height = 200;
+
+    Vector2 player1_pos = {100, 100};
+    Vector2 player2_pos = {GetScreenWidth() - 100.f, GetScreenHeight() + 100.f};
+
     InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
     SetTargetFPS(60);
 
@@ -23,26 +29,26 @@ int main () {
         ball_position.x += ball_velocity.x * ball_speed;
         ball_position.y += ball_velocity.y * ball_speed;
 
-        cout << ball_position.x + ball_size << endl;
-
-        if (ball_position.x + ball_size >= GetScreenWidth()){
+        if (ball_position.x + ball_size >= GetScreenWidth() || ball_position.x - ball_size <= 0){
             ball_velocity.x *= -1;
         }
 
-        if (ball_position.x - ball_size <= 0){ //is - beacause I need to go up to check;
-            ball_velocity.x *= -1;
-        }
-
-        if (ball_position.y + ball_size >= GetScreenHeight()){
+        if (ball_position.y + ball_size >= GetScreenHeight() || ball_position.y - ball_size <= 0){
             ball_velocity.y *= -1;
         }
 
-        if (ball_position.y - ball_size <= 0){ //is - beacause I need to go up to check;
-            ball_velocity.y *= -1;
-        }
-
-        DrawText("Hello world!", 400, 300, 32, BLACK);
+        DrawText("Hello world!", GetScreenWidth() / 2, GetScreenHeight() / 2, 32, BLACK);
         DrawCircle(ball_position.x, ball_position.y, ball_size, BLACK);
+
+        DrawRectangle(player1_pos.x, player1_pos.y, rect_width, rect_height, GREEN);
+        DrawRectangle(player2_pos.x, player2_pos.y, rect_width, rect_height, RED);
+
+        if (IsKeyDown(KEY_S) && player1_pos.y + rect_height < GetScreenHeight()) {
+            player1_pos.y += 10;
+        }else if (IsKeyDown(KEY_W) && player1_pos.y > 0){
+            player1_pos.y -= 10;
+        }
+
         EndDrawing();
     }
 
