1df2f7250994708ff960b5b1c9084a919ff30f05
Vinicius Antonio da Silva <vinicius.antonio0712@gmail.com>
2023-12-05 22:09:10 -0300

    Finish core game loop

--- main.cpp
+++ main.cpp
@@ -3,13 +3,40 @@
 
 using namespace std;
 
+bool CheckCollision(Vector2 object1_position, Vector2 object1_dimentions, Vector2 object2_position, Vector2 object2_dimentions){
+    if (object2_position.x < object1_position.x + object1_dimentions.x && 
+        object2_position.y < object1_position.y + object1_dimentions.y &&
+        object2_position.y + object2_dimentions.y > object1_position.y &&
+        object2_position.x + object2_dimentions.x > object1_position.x 
+        ){
+        return true;
+    }
+}
+
+void DrawTextPivotCenter(const char *text, int fontSize, Vector2 position){
+    // Configurando o texto
+    // const char *text = "Hello, World!";
+    // int fontSize = 20;
+    Vector2 textSize = MeasureTextEx(GetFontDefault(), text, fontSize, 1);
+
+    // Calculando a posição para centralizar o texto
+    float x = (position.x - textSize.x) / 2;
+    float y = (position.y - textSize.y) / 2;
+
+    // Renderizando o texto no meio da tela
+    DrawText(text, x, y, fontSize, BLACK);
+}
+
 int main () {
 
     const int screenWidth = 800;
     const int screenHeight = 600;
 
+    int p1_score = 0;
+    int p2_score = 0;
+
     int ball_size = 30;
-    double ball_speed = 5.f;
+    double ball_speed = 7.f;
     Vector2 ball_position = {200, 200};
     Vector2 ball_velocity = {1, 1};
 
@@ -17,7 +44,7 @@ int main () {
     int rect_height = 200;
 
     Vector2 player1_pos = {100, 100};
-    Vector2 player2_pos = {GetScreenWidth() - 100.f, GetScreenHeight() + 100.f};
+    Vector2 player2_pos = {700, 100};
 
     InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
     SetTargetFPS(60);
@@ -37,8 +64,17 @@ int main () {
             ball_velocity.y *= -1;
         }
 
-        DrawText("Hello world!", GetScreenWidth() / 2, GetScreenHeight() / 2, 32, BLACK);
+        std::string scoreText =  std::to_string(p1_score) + " : " + std::to_string(p2_score);
+        const char* pScoreText = scoreText.c_str();
+        DrawTextPivotCenter(pScoreText, 32, {(float)GetScreenWidth(), (float)GetScreenHeight()}); // isso é o equivalente a p1_score + " : " + p2_score
         DrawCircle(ball_position.x, ball_position.y, ball_size, BLACK);
+        
+        //Debug Ball collision
+        // DrawRectangle(ball_position.x - ball_size, ball_position.y - ball_size, ball_size * 2, ball_size * 2, BLUE);
+
+        //Debug Score Rects
+        // DrawRectangle(0, 0, 100, GetScreenHeight(), BLUE); //player1_goal
+        // DrawRectangle(GetScreenWidth() - 70, 0, 100, GetScreenHeight(), PINK); //player2_goal
 
         DrawRectangle(player1_pos.x, player1_pos.y, rect_width, rect_height, GREEN);
         DrawRectangle(player2_pos.x, player2_pos.y, rect_width, rect_height, RED);
@@ -47,6 +83,31 @@ int main () {
             player1_pos.y += 10;
         }else if (IsKeyDown(KEY_W) && player1_pos.y > 0){
             player1_pos.y -= 10;
+        }
+
+        if (IsKeyDown(KEY_DOWN) && player2_pos.y + rect_height < GetScreenHeight()) {
+            player2_pos.y += 10;
+        }else if (IsKeyDown(KEY_UP) && player2_pos.y > 0){
+            player2_pos.y -= 10;
+        }
+
+        //The flickering glith is caused because of the check every frame
+        if(CheckCollision(player1_pos, {(float)rect_width, (float)rect_height}, {ball_position.x - ball_size, ball_position.y - ball_size}, {(float)ball_size * 2, (float)ball_size * 2})){
+            ball_velocity.x *= -1;
+        }
+        if(CheckCollision(player2_pos, {(float)rect_width, (float)rect_height}, {ball_position.x - ball_size, ball_position.y - ball_size}, {(float)ball_size * 2, (float)ball_size * 2})){
+            ball_velocity.x *= -1;
+        }
+
+        if(CheckCollision({0, 0}, {100.f, (float)GetScreenHeight()}, {ball_position.x - ball_size, ball_position.y - ball_size}, {(float)ball_size * 2, (float)ball_size * 2})){
+            ball_position = {(float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2};
+            p2_score ++;
+            ball_velocity.x = -1;
+        }
+        if(CheckCollision({(float)GetScreenWidth() - 70, 0}, {100, (float)GetScreenHeight()}, {ball_position.x - ball_size, ball_position.y - ball_size}, {(float)ball_size * 2, (float)ball_size * 2})){
+            ball_position = {(float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2};
+            p1_score ++;
+            ball_velocity.x = 1;
         }
 
         EndDrawing();
